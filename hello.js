9b1d68b135f25c3affbe9f4cdabe1710f6571271
Muhammed <mohammadjafer990@gmail.com>
2023-06-18 00:02:10 +0300

    events are done

--- hello.js
+++ hello.js
@@ -1,7 +1,36 @@
-const mission = 'learn';
+// setTimeout(() => {
+//     console.log('hello world');
+// }, 3000);
+// console.log("hello world");
+// console.log
 
-if (mission === "learn") {
-    console.log("time to write node code ");
-} else {
-    console.log("is not a mission???");
-}
+//events in node.js are mentioned as below
+
+// const EventEmitter = require('events');
+// const celebrity = new EventEmitter();
+
+// // Subscribe to celebrity for Observer 1
+// celebrity.on('race win', () => {
+//     console.log('congratse you are the best in the race');
+// });
+
+// celebrity.on('race win', () => {
+//     console.log('i am better than you think you are');
+// });
+
+
+// celebrity.emit('race win');
+
+// make http request in node.js
+
+const http = require('http');
+
+const req = http.request('http://www.google.com', (res) => {
+    res.on('data', (chunk) => {
+        console.log(`data chunk is a piece of data: ${chunk}`);
+        });
+    res.on('end', () => {console.log('no more data'); 
+    });
+});
+
+req.end();
